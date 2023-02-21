90155c99c9f288bf19b463c6128b0e5b950c23e4
benji <you@example.com>
2023-02-21 14:16:56 -0800

    slower fade

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ document.addEventListener("touchstart", function(event) {
       `;
       textBox.setAttribute("data-index", currentIndex);
       textBox.classList.remove("fade");
-    }, 500);
+    }, 750);
   }, false);
   
